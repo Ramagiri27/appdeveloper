17b71e1b5b6205c07b740910294db4ffa8424f35
Ramagiri27 <148475266+Ramagiri27@users.noreply.github.com>
2023-10-20 11:58:57 +0530

    Add files via upload

---
+++ flutter1.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.awt.*;
+public class flutter1 {
+    Flutter1(){
+    Button b =new Button("Click");
+        Frame f=new Frame();
+        f.add(b);
+        f.setlayout(null);
+        f.setSize(300,300);
+        f.setVisible(true);
+ }
+     public static void main (String[]args ){
+     Flutter1 f1=new Flutter1();
+    }
+}
+
+    
+
